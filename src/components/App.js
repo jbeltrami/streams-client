e3925de99f27763a7b61ee418559cf76b8a99e05
jbeltrami <joao_beltrami@hotmail.com>
2019-07-12 08:54:59 -0400

    Create routes to map each of the Stream Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => (
-  <div>
-    <Link to="/pagetwo">Navigate to Page Two</Link>
-    PageOne
-  </div>
-);
-
-const PageTwo = () => (
-  <div>
-    PageTwo
-    <Link to="/">Navigate to Page One</Link>
-    <button type="button">click me</button>
-  </div>
-);
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => (
   <div>
     <BrowserRouter>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+        <Route path="/" exact component={StreamList}></Route>
+        <Route path="/streams/new" exact component={StreamCreate}></Route>
+        <Route path="/streams/edit" exact component={StreamEdit}></Route>
+        <Route path="/streams/delete" exact component={StreamDelete}></Route>
+        <Route path="/streams/show" exact component={StreamShow}></Route>
       </div>
     </BrowserRouter>
   </div>
