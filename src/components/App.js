9923447ab85b903e378401d5b815beed58467622
jbeltrami <joao_beltrami@hotmail.com>
2019-07-12 08:42:09 -0400

    Start experimenting with React Router Dom
    User BrowserRouter, Route and Link tags to understand how React can
    handle routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+const PageOne = () => (
+  <div>
+    <Link to="/pagetwo">Navigate to Page Two</Link>
+    PageOne
+  </div>
+);
+
+const PageTwo = () => (
+  <div>
+    PageTwo
+    <Link to="/">Navigate to Page One</Link>
+    <button type="button">click me</button>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
