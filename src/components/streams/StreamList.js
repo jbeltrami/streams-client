2dcc65bbc33c839e41e2c998fd95910fe1daa192
jbeltrami <joao_beltrami@hotmail.com>
2019-07-16 15:36:54 -0400

    Add logic to verify Stream object's owner
    Modify createStream action, to be able to read the auth object.
    Use that object to verify stream's owner.
    Create a helper method inside StreamList component to verify if current user's id is the same as the stream creator and if true, display delete/edit buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => <div>StreamList</div>;
+class StreamList extends Component {
+  componentDidMount() {
+    const { fetchStreams: getList } = this.props;
 
-export default StreamList;
+    getList();
+  }
+
+  renderAdmin(stream) {
+    const { currentUserId } = this.props;
+
+    if (stream.userId === currentUserId)
+      return (
+        <div className="right floated content">
+          <button className="ui button primary" type="button">
+            Edit
+          </button>
+          <button className="ui button negative" type="button">
+            Delete
+          </button>
+        </div>
+      );
+  }
+
+  renderList() {
+    const { streams } = this.props;
+    return streams.map(stream => (
+      <div className="item" key={stream.id}>
+        {this.renderAdmin(stream)}
+        <i className="large middle aligned icon camera"></i>
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  streams: Object.values(state.streams),
+  currentUserId: state.auth.userId,
+});
+
+StreamList.propTypes = {
+  fetchStreams: PropTypes.func,
+  streams: PropTypes.array,
+  currentUserId: PropTypes.string,
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
