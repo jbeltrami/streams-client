f12b8a72af3ac7ed11fb64a4d33fa4db8108776c
jbeltrami <joao_beltrami@hotmail.com>
2019-07-19 13:28:58 -0400

    Add component for StreamShow and connect it to Redux Store.

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    const { fetchStream: getStream, match } = this.props;
 
-export default StreamShow;
+    getStream(match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) return <div>Loading...</div>;
+
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id],
+});
+
+StreamShow.propTypes = {
+  fetchStream: PropTypes.func,
+  match: PropTypes.object,
+  stream: PropTypes.object,
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
